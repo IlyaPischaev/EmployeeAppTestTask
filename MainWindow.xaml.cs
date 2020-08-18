120e21f0a3a3334f3268b302999ccd8d565a6091
SarumanTheJoker <48750104+LGBTLoverBoy@users.noreply.github.com>
2020-08-18 22:32:52 +0500

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmployeeAppTestTask
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            EmployeeDataEntities employeeContex = new EmployeeDataEntities();
+
+            employeeContex.Employees.Load();
+            employeeGrid.ItemsSource = employeeContex.Employees.Local.ToBindingList();
+        }
+
+        private void Button_AddEmployee(object sender, RoutedEventArgs e)
+        {
+            WindowAddEmployee windowAddEmployee = new WindowAddEmployee(this);
+
+            windowAddEmployee.Show();
+
+            if (!windowAddEmployee.IsActive)
+            {
+                employeeGrid.Items.Refresh();
+            }
+        }
+        private void Button_DeleteEmployee(object sender, RoutedEventArgs e)
+        {
+            if (employeeGrid.SelectedCells.Count == 0)
+            {
+                MessageBox.Show("Выделите сотрудника, которого хотите удалить.");
+            }
+            else
+            {
+                using (EmployeeDataEntities employeeContex = new EmployeeDataEntities())
+                {
+                    Employee selectedItem = employeeGrid.SelectedItem as Employee;
+
+                    Employee employee = employeeContex.Employees.FirstOrDefault(item => item.Id == selectedItem.Id);
+
+                    employeeContex.Employees.Remove(employee);
+                    employeeContex.SaveChanges();
+
+                    employeeGrid.Items.Refresh();
+
+                    MessageBox.Show("Сотрудник удалён");
+                }
+            }
+        }
+    }
+}
---
+++ WindowAddEmployee.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.Entity;
+using System.ComponentModel;
+
+namespace EmployeeAppTestTask
+{
+    /// <summary>
+    /// Interaction logic for WindowAddEmployee.xaml
+    /// </summary>
+    public partial class WindowAddEmployee : Window
+    {
+        private MainWindow _mainWindow;
+
+        public delegate void UpdateData();
+        public WindowAddEmployee(MainWindow mainWindow)
+        {
+            InitializeComponent();
+
+            _mainWindow = mainWindow;
+        }
+
+        public void Button_AddEmployeeConfirm(object sender, RoutedEventArgs e)
+        {          
+            using (EmployeeDataEntities employeeContex = new EmployeeDataEntities())
+            {
+                var name = TexBox_Name.Text;
+                var surname = TextBox_Surname.Text;
+                var thirdName = TextBox_ThirdName.Text;
+
+                bool isEmpty = (name == "" ^ surname == "" ^ thirdName == "");
+                if (isEmpty)
+                {
+                    MessageBox.Show("Одно из полей пустое!");
+                }
+                else
+                {
+                    Employee employee = new Employee()
+                    {
+                        Name = name,
+                        Surname = surname,
+                        ThirdName = thirdName
+                    };
+
+                    employeeContex.Employees.Add(employee);
+                    employeeContex.SaveChanges();
+
+                    MessageBox.Show($"Пользователь {employee.Surname} {employee.Name} {employee.ThirdName} успешно добавлен!");
+
+                    _mainWindow.employeeGrid.Items.Refresh();
+                }
+            }
+
+            _mainWindow.employeeGrid.Items.Refresh();
+            this.Close();
+        }
+
+        void Closing_WindowAppEmployee(object sender, CancelEventArgs e)
+        {
+            _mainWindow.employeeGrid.Items.Refresh();
+        }
+    }
+}
